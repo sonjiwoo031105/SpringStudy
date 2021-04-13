a337469148e55cf30bda9b45661fcb0e2c9ff363
Jiwoo Son <jiji432@naver.com>
2021-04-13 18:02:44 +0900

    0413

---
+++ AOP2/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,19 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import kr.hs.study.beans.TestBean1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+		TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+		obj1.method1();
+		obj1.method2();
+		
+		ctx.close();
+	}
+
+}
